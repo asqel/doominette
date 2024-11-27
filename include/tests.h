9ecd9a2905698b8faa13c88a371c4cd16926f105
Dracorex2 <lucas.m3166@gmail.com>
2024-11-27 19:36:50 +0100

    parse

--- include/tests.h
+++ include/tests.h
@@ -2,12 +2,15 @@
 #define TESTS_H
 
 #include  <dlfcn.h>
+#include "types.h"
 
 typedef struct {
 	char *dir_path;
 	char *dir_name;
 	char *ex_name; // standardized (ex C00 -> c00)
 	void *lib_handle;
+	char **stdout;
+	u32 stdout_len;
 } test_info_t;
 
 #endif
---
+++ tests/c00/test_c00.c
@@ -0,0 +1,15 @@
+#include "tests.h"
+#include <dlfcn.h>
+
+void	tst_put_char(char c, void (*ft_putchar)(char c))
+{
+	
+}
+
+void	tester(test_info_t info)
+{
+
+	void (*ft_putchar)(char c) = dlsym(info.lib_handle, "ft_putchar");
+	(*ft_putchar)('c')
+
+}
