3bd4c4550e0afe499d53496cda0c5382ac81dd33
asqel <axel.lleres@gmail.com>
2024-11-27 19:19:18 +0100

    update

--- include/tests.h
+++ include/tests.h
@@ -1,10 +1,13 @@
 #ifndef TESTS_H
 #define TESTS_H
 
+#include  <dlfcn.h>
+
 typedef struct {
 	char *dir_path;
 	char *dir_name;
 	char *ex_name; // standardized (ex C00 -> c00)
+	void *lib_handle;
 } test_info_t;
 
 #endif
