a3207d380e67f817f9f04bfae3832f62adc81e8e
asqel <axel.lleres@gmail.com>
2024-11-28 19:11:03 +0100

    log_printf

--- src/utils/logs.c
+++ src/utils/logs.c
@@ -1,9 +1,25 @@
 #include "logs.h"
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 char *logs = NULL;
 
 void write_log(char *log) {
 	logs = realloc(logs, strlen(logs) + strlen(log) + 1);
 	strcat(logs, log);
+}
+
+void log_printf(char *str, ...)	{
+	va_list args;
+	va_list args2;
+	va_start(args, str);
+	va_copy(args2, args);
+	int len = vsprintf(NULL, str, args);
+
+	logs = realloc(logs, strlen(logs) + len + 1);
+	vsprintf(&(logs[strlen(logs)]), str, args2);
+
+	va_end(args);
+	va_end(args2);
 }
